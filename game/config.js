e62c44cdb5648024370972d14935a66fe068e513
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2024-05-08 20:42:20 +0800

    Oops, delete one more line.

--- game/config.js
+++ game/config.js
@@ -130,6 +130,7 @@ window.config = {
 
 	cards: ["standard", "extra", "sp", "zhulu", "yingbian", "yongjian", "yunchou"],
 	characters: ["standard", "shenhua", "sp", "sp2", "yijiang", "refresh", "xinghuoliaoyuan", "mobile", "extra", "yingbian", "sb", "tw", "offline", "clan", "collab", "xianding", "huicui", "shiji", "jsrg", "onlyOL"],
+	moderned_chracters: ["standard", "shenhua", "sp", "sp2", "yijiang", "refresh", "xinghuoliaoyuan", "mobile", "extra", "yingbian", "sb", "tw", "offline", "clan", "collab", "xianding", "huicui", "shiji", "onlyOL", "jsrg", "old", "diy", "ddd", "key"],
 	connect_characters: ["diy", "ddd"],
 	connect_cards: ["huanlekapai", "guozhan", "sp", "zhulu", "yingbian", "yongjian", "yunchou"],
 	plays: [],
